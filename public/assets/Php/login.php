eb955ee11b096342ff65903ec9e81d8508107583
ArthurMorgan <lcda72@gmail.com>
2026-05-07 12:15:57 -0600

    Update login.php

--- public/assets/Php/login.php
+++ public/assets/Php/login.php
@@ -1,24 +1,11 @@
-<?php
-require_once 'db.php';
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $correo = $_POST['correo'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM USUARIO WHERE correo = ? AND estado = 'activo'";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$correo]);
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user && password_verify($password, $user['contrasena_hash'])) {
-        $_SESSION['id_usuario'] = $user['id_usuario'];
-        $_SESSION['nombre'] = $user['nombre_completo'];
-
-        echo "Login exitoso";
-    } else {
-        echo "Credenciales incorrectas";
-    }
-}
-?>
+<form action="login.php" method="POST">
+    <div class="input-group">
+        <i class="fa-solid fa-envelope icon-left"></i>
+        <input type="email" name="correo" placeholder="Correo electrónico" required>
+    </div>
+    <div class="input-group">
+        <i class="fa-solid fa-lock icon-left"></i>
+        <input type="password" name="password" placeholder="Contraseña" required>
+    </div>
+    <button type="submit" class="submit-btn">Entrar</button>
+</form>
