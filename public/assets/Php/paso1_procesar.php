dc3488367e4552610aca2e781fedd23d811aac1a
Breakflip <153700442+Breakflip@users.noreply.github.com>
2026-05-21 11:18:12 -0600

    Delete public/assets/Php directory

--- public/assets/Php/paso1_procesar.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $_SESSION['temp_reg'] = $_POST;
-    header("Location: datos_personales.html");
-    exit();
-}
--- public/assets/Php/register.php
+++
@@ -1,7 +0,0 @@
-<form action="paso1_procesar.php" method="POST">
-    <input type="email" name="correo" placeholder="Correo electrónico" required>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <input type="text" name="curp" placeholder="CURP" maxlength="18" required>
-    <input type="tel" name="telefono" placeholder="Número de teléfono" required>
-    <button type="submit" class="submit-btn">Siguiente</button>
-</form>
